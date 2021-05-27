680bd6bc83f7a873a29d8df924177c0ce838050c
Serghei Iakovlev <egrep@protonmail.ch>
2021-05-27 03:09:49 +0300

    Fix db_url_config to work the same for all postgres aliases

--- tests/test_db.py
+++ tests/test_db.py
@@ -111,6 +111,46 @@ def test_db_parsing(url, engine, name, host, user, passwd, port):
         assert config['HOST'] == host
 
 
+def test_postgres_complex_db_name_parsing():
+    """Make sure we can use complex postgres host."""
+    env_url = ('postgres://user:password@//cloudsql/'
+               'project-1234:us-central1:instance/dbname')
+    url = Env.db_url_config(env_url)
+
+    assert url['ENGINE'] == DJANGO_POSTGRES
+    assert url['HOST'] == '/cloudsql/project-1234:us-central1:instance'
+    assert url['NAME'] == 'dbname'
+    assert url['USER'] == 'user'
+    assert url['PASSWORD'] == 'password'
+    assert url['PORT'] == ''
+
+
+@pytest.mark.parametrize(
+    'scheme',
+    ['postgres', 'postgresql', 'psql', 'pgsql', 'postgis'],
+)
+def test_postgres_parsing_with_scheme_aliases(scheme):
+    """Verify all the Postgres aliases parse the same as postgres."""
+    env_url = ('postgres://user:password@//cloudsql/'
+               'project-1234:us-central1:instance/dbname')
+    url = Env.db_url_config(env_url)
+
+    env_url2 = ('{}://user:password@//cloudsql/'
+                'project-1234:us-central1:instance/dbname').format(scheme)
+    url2 = Env.db_url_config(env_url2)
+
+    assert url2['NAME'] == url['NAME']
+    assert url2['PORT'] == url['PORT']
+    assert url2['PASSWORD'] == url['PASSWORD']
+    assert url2['USER'] == url['USER']
+    assert url2['HOST'] == url['HOST']
+
+    if scheme == 'postgis':
+        assert url2['ENGINE'] == 'django.contrib.gis.db.backends.postgis'
+    else:
+        assert url2['ENGINE'] == url['ENGINE']
+
+
 def test_memory_sqlite_url_warns_about_netloc(recwarn):
     warnings.simplefilter("always")
 
